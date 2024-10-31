6434a365d60ae9460b4d66fde12cc07e9012566f
Oleh Skrypnyk-Zakrevskiy <awallencecode@gmail.com>
2024-10-31 23:34:13 +0200

    Task 1 from hm is done

---
+++ main.py
@@ -0,0 +1,9 @@
+red = '\033[91m'
+gray = '\033[90m'
+reset = '\033[0m'
+
+def print_quotation():
+    print(f"\n{gray}\"Don't compare yourself with anyone in this world…{reset}")
+    print(f"{red}if{reset} you {red}do so{reset}, you are insulting yourself.{gray}\"{reset}\nBill Gates")
+
+print_quotation()

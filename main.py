bbbed3a96c56ec7e6ca428d3908f74a35863cba9
Oleh Skrypnyk-Zakrevskiy <awallencecode@gmail.com>
2024-11-01 22:53:10 +0200

    Task 5 from hm is done

--- main.py
+++ main.py
@@ -1,27 +1,37 @@
-def _min_of_five(num_1 = 3, num_2 = 7, num_3 = -3, num_4 = 0, num_5 = 11):
-    numbers = [num_1, num_2, num_3, num_4, num_5]
-    my_min_of_five = numbers[0]
-
-    for number in numbers:
-        if number  < my_min_of_five:
-            my_min_of_five = number
-
-    print(my_min_of_five)
-
-_min_of_five()
-
-# The second option is for the crazies :)
-
-# def _min_of_five(num_1 = 8, num_2 = 3, num_3 = 1, num_4 = -2, num_5 = 0):
-#     if num_1 < num_2 and num_1 < num_3 and num_1 < num_4 and num_1 < num_5:
-#         print(num_1)
-#     elif num_2 < num_1 and num_2 < num_3 and num_2 < num_4 and num_2 < num_5:
-#         print(num_2)
-#     elif num_3 < num_1 and num_3 < num_2 and num_3 < num_4 and num_3 < num_5:
-#         print(num_3)
-#     elif num_4 < num_1 and num_4 < num_2 and num_4 < num_3 and num_4 < num_5:
-#         print(num_4)
-#     else:
-#         print(num_5)
+# var 1
+# def product_diapazone (num_1 = 25, num_2 = 5):
+#     """function multiplies numbers in the range"""
+#     product = 1
 #
-# _min_of_five()
+#     if num_1 < num_2:
+#         for num in range (num_1, num_2 + 1):
+#             product *= num
+#         print(product)
+#
+#     else:
+#         num_1, num_2 = num_2, num_1
+#         for num in range (num_1, num_2 + 1): #the values num_1 and num_2 of the variables can not be changed, but simply swap num_1 and num_2 in the stipulation loop
+#             product *= num
+#         print(product)
+#
+# product_diapazone()
+
+# var 2
+def product_diapazone (num_1 = 25, num_2 = 5):
+    """function multiplies numbers in the range"""
+    if num_1 > num_2:
+        num_1, num_2 = num_2, num_1
+
+    product = 1
+
+    for num in range (num_1, num_2 + 1):
+        product *= num
+
+    return product
+
+result = product_diapazone()
+
+print(result)
+
+# In this variant I shortened and improved the code, and also corrected the problem condition,
+# where it was said that it was necessary to return and not to output the result.

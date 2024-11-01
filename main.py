fc833dc9585721380fa3531ac1e08fe93b4c445f
Oleh Skrypnyk-Zakrevskiy <awallencecode@gmail.com>
2024-11-01 16:19:04 +0200

    Task 3 from hm is done

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-def all_even_numbers_between_them (num_1, num_2):
-    for i in range(num_1, num_2):
-        if i % 2 == 0:
-            print(i, end = " | ")
+def print_square (symbol, side, variation):
+    if variation == True:
+        for i in range (side):
+            print((symbol + ' ') * side) # I'm using + ' ' it to make the square more readable
+    elif variation == False:
+        for i in range (side):
+            if i == 0 or i == side - 1:
+                print(symbol * side )
+            else:
+                print(symbol + ' ' * (side - 2) + symbol)
+    else:
+        print('Invalid variation, try again')
 
-all_even_numbers_between_them(2,50)
+print_square('*',5,True)

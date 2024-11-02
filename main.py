75f0de11f7f7f5d0a2d56d877af426825d786e3d
Oleh Skrypnyk-Zakrevskiy <awallencecode@gmail.com>
2024-11-02 17:42:16 +0200

    Var_1 and Var_2 in Task 7 from hm is done

--- main.py
+++ main.py
@@ -1,27 +1,31 @@
 #var 1
 
-# def polindrom (number = 123321):
-#     number = str(number)
-#
-#     mas_reverse = []
-#     mas = []
-#
-#     for letter in number:
-#         mas.append(letter)
-#         mas_reverse.append(letter)
-#
-#     mas_reverse.reverse()
-#
-#     if mas == mas_reverse:
-#         return True
-#     else:
-#         return False
-#
-# result = polindrom()
-#
-# print(result)
+def polindrom (number = 123321):
+    """palindrome search function"""
+    number = str(number)
+
+    mas_reverse = []
+    mas = []
+
+    for letter in number:
+        mas.append(letter)
+        mas_reverse.append(letter)
+
+    mas_reverse.reverse()
+
+    if mas == mas_reverse:
+        return True
+    else:
+        return False
+
+result = polindrom()
+
+print(result)
+
+#var 2 "mathematical solution"
 
 def is_polindrom (number = -546645):
+    """palindrome search function"""
     if number < 0:
         number = abs(number) #check for a negative number and its inerting
         #or return False
